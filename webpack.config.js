fc13a79c0df25dc183e6a5bf824c4d7d372813d1
akshas <allexxx81@gmail.com>
2020-01-17 15:59:00 +0100

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlParser = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
@@ -14,10 +15,19 @@ module.exports = {
     devServer: {
         port: 3000
     },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        }, ],
+    },
     plugins: [
         new HtmlParser({
             template: "index.html"
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
     ]
 }
