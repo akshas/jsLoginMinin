986da2c670463df6546d8fcae52f955d91a0a705
akshas <allexxx81@gmail.com>
2020-01-22 15:31:33 +0100

    firebase connected

--- questions.js
+++ questions.js
@@ -1,13 +1,27 @@
 export class Question {
   static create(question) {
-    return fetch("https://podcast-a290f.firebaseio.com/question.json", {
-      method: "POST",
-      body: JSON.stringify(question),
-      headers: {
-        "Content-type": "application/json"
-      }
-    })
-      .then(response => response.json)
-      .then(response => console.log(response));
+    return fetch("https://questions-632dd.firebaseio.com/questions.json", {
+        method: "POST",
+        body: JSON.stringify(question),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        question.id = response.name;
+        return question;
+      })
+      .then(addToLocalStorage)
   }
+}
+
+function addToLocalStorage(question) {
+  let all = getQuestionsFromLocalStorage();
+  all.push(question);
+  localStorage.setItem('questions', JSON.stringify(all));
+}
+
+function getQuestionsFromLocalStorage() {
+  return JSON.parse(localStorage.getItem('questions') || "[]");
 }
